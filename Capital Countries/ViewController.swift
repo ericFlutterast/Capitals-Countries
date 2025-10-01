ed0011979db36fdbcbc3f3305a4fa245208a464e
driller <erikGitFlutter@gmail.com>
2025-10-01 21:54:09 +0300

    Initial Commit

---
+++ Capital Countries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Capital Countries
+//
+//  Created by Эрик Нугзарович on 10/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
